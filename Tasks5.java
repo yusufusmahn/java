acb0534d25aca5b4d4bbee2b49100cae91b68920
yusufusmahn <yusuf.usman1003@gmail.com>
2024-12-07 13:48:11 +0100

    Add files via upload

---
+++ Tasks5.java
@@ -0,0 +1,18 @@
+public class Tasks5 {
+	public static void main(String[] args) {
+
+	for (int count =2; count <= 10; count++) {
+	if (count %4 == 0){
+
+	for (int j = 1; j <= 5; j++){
+	System.out.print(count);
+
+}
+}
+}
+
+
+}
+
+	}
+	
---
+++ Tasks6.java
@@ -0,0 +1,20 @@
+public class Tasks6 {
+	public static void main(String[] args) {
+
+	int four =4;
+	int eight = 8;
+	int multipleFour =1;
+	int multipleEight =1;
+	
+	for (int count =0; count < 5; count++) {
+
+	multipleFour = multipleFour * four;
+
+	multipleEight = multipleEight * eight;
+
+	System.out.println(multipleFour);
+	System.out.println(multipleEight);
+
+}
+}
+	}
