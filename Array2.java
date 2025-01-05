ca61aaf9d7bfbb1a9ef082c2d2b2e7b064e049d3
yusufusmahn <yusuf.usman1003@gmail.com>
2025-01-05 20:07:02 +0100

    Update Array2.java

--- Array2.java
+++ Array2.java
@@ -1,18 +1,17 @@
-import java.util.List;
 import java.util.ArrayList;
 import java.util.Collections;
 
 public class Array2{
 public static void main(String[] args){
 
-List<Integer> figures = new ArrayList<>();
+ArrayList<Integer> figures = new ArrayList<>();
 figures.add(1);
 figures.add(2);
 figures.add(3);
 figures.add(4);
 figures.add(5);
 
-List<Integer> result = reversal (figures);
+ArrayList<Integer> result = reversal (figures);
 
 
 System.out.println("The reversed list of "+result);
@@ -21,9 +20,9 @@ System.out.println("The reversed list of "+result);
 }
 
 
-public static List<Integer> reversal (List<Integer>figures){
+public static ArrayList<Integer> reversal (ArrayList<Integer>figures){
 
-List<Integer> reversedFigures = new ArrayList<>(figures);
+ArrayList<Integer> reversedFigures = new ArrayList<>(figures);
 Collections.reverse(reversedFigures);
 return reversedFigures;
 
